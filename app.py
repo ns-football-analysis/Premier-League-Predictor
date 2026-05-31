21d44c2f3d7622a8aeede129a5c7a4060b841728
ns-football-analysis <c7h7chcmy7@privaterelay.appleid.com>
2026-05-31 15:33:20 +0100

    Revise welcome message in app.py
    Updated the welcome message to specify 'Premier league predictor'.

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="PL Analytics Hub", page_icon="🏠", layout="wide
 st.sidebar.success("Select a dashboard above.") 
 
 st.write("# 🏆 Premier League Prediction Hub")
-st.write("Welcome to your data science workspace. Use the sidebar menu on the left to toggle features:")
+st.write("Welcome to your Premier league predictor. Use the sidebar menu on the left to toggle features:")
 st.write("* **1 Match Predictor:** Run individual head-to-head fixtures.")
 st.write("* **2 Season Simulator:** Run simulations to project the final league table standings.")
 st.info("👈 Open the sidebar menu on the left to select an application sheet!")
